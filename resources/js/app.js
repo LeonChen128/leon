b17f0c75d4d9e5d2cf22fe889f7d96f8fb788c93
陳進益 <a781282000@gmail.com>
2023-07-30 15:32:57 +0800

    安裝 sass

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+
+console.log(123)
+
+function pow(num) {
+    return num * num
+}
+
+const hello = str => console.log('hello ' + str)
+
+hello('Leon')
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,17 @@
 <html>
 <head>
     <title></title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 </head>
 <body>
 
     <h1>Hello World!</h1>
+
+    <div class="test">
+        <div>123</div>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
 
 </body>
 </html>
